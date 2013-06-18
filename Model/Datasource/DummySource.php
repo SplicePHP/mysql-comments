147097598b2ff5b6d44bcac939acaad28bbcf22e
Dieter Gribnitz <admin@splicephp.com>
2013-06-18 04:54:07 +0200

    Forgot to load this one with initial upload

---
+++ Model/Datasource/DummySource.php
@@ -0,0 +1,18 @@
+<?php
+class DummySource extends DataSource {
+
+    function connect() {
+        $this->connected = true;
+        return $this->connected;
+    }
+
+    function disconnect() {
+        $this->connected = false;
+        return !$this->connected;
+    }
+
+    function isConnected() {
+
+        return true;
+    }
+}
